51074e7f6a235b5b24d42e7e4ef2e10ce255c831
Matthew Whisenhunt <matt.whisenhunt@gmail.com>
2018-09-03 12:14:48 -0700

    Add a PHP-based, in-repo generator.

---
+++ generator/_Service.php
@@ -0,0 +1,176 @@
+<?php
+
+echo '<?php';
+?>
+
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/**
+ * Service definition for <?=$CapCan?> (<?=$doc['version']?>).
+ *
+ * <p>
+ * <?=wordwrap(trim($doc['description']), 77, "\n * ")?></p>
+ *
+ * <p>
+ * For more information about this service, see the API
+ * <a href="<?=$doc['documentationLink']?>" target="_blank">Documentation</a>
+ * </p>
+ *
+ * @author Google, Inc.
+ */
+class Google_Service_<?=$CapCan?> extends Google_Service
+{
+<? if ($real_scopes): ?>
+<? foreach ($real_scopes as $key => $value): ?>
+  /** <?=$value[1]?>. */
+  const <?=$key?> =
+      "<?=$value[0]?>";
+<? endforeach; ?>
+<? else: ?>
+
+<? endif; ?>
+
+<? if ($all_resources)
+   foreach ($all_resources as $key => $value): ?>
+  public $<?=lcfirst($key)?>;
+<? endforeach; ?>
+<? if ($doc['methods']): ?>
+  private $base_methods;
+<? else: ?>
+  
+<? endif; ?>
+  /**
+   * Constructs the internal representation of the <?=wordwrap("$CapCan service", 28, "\n   * ")?>.
+   *
+   * @param Google_Client $client
+   */
+  public function __construct(Google_Client $client)
+  {
+    parent::__construct($client);
+    $this->rootUrl = '<?=$doc['rootUrl']?>';
+    $this->servicePath = '<?=$doc['servicePath']?>';
+    $this->version = '<?=$doc['version']?>';
+    $this->serviceName = '<?=$doc['name']?>';
+
+<? if ($all_resources)
+   foreach ($all_resources as $class_name => $keys): // resource
+     $node = getResourceReference($keys);
+     $res_class_name = render_resource_name($keys);
+     ksort($node['methods']); ?>
+    $this-><?=lcfirst($class_name)?> = new Google_Service_<?=$CapCan?>_Resource_<?=$res_class_name?>(
+        $this,
+        $this->serviceName,
+        '<?=$keys[count($keys)-1]?>',
+        array(
+          'methods' => array(
+            <? foreach ($node['methods'] as $method_name => $method): // resource,method ?>
+'<?=$method_name?>' => array(
+              'path' => '<?=$method['path']?>',
+              'httpMethod' => '<?=$method['httpMethod']?>',
+<?php
+   if ($method['parameterOrder']) {
+     $parameters = [];
+     foreach ($method['parameterOrder'] as $movedKey) {
+       $parameters[$movedKey] = $method['parameters'][$movedKey];
+       unset($method['parameters'][$movedKey]);
+     }
+     $method['parameters'] = array_merge($parameters, $method['parameters']);
+   }
+   if ($method['parameters']): ?>
+              'parameters' => array(<?foreach ($method['parameters'] as $pname => $pval): // resource,method,parameter?>
+
+                '<?=$pname?>' => array(
+                  'location' => '<?=$pval['location']?>',
+                  'type' => '<?=$pval['type']?>',
+<? if ($pval['repeated']):?>
+                  'repeated' => true,
+<? endif;?>
+<? if ($pval['required']):?>
+                  'required' => true,
+<? endif;?>
+                ),<? endforeach; // resource,method,parameter ?>
+
+              ),
+<? else: ?>
+              'parameters' => array(),
+<? endif; ?>
+            ),<? endforeach; // resource,method ?>
+
+          )
+        )
+    );
+<? endforeach; // resource ?>
+<? if ($doc['methods']): ?>
+    $this->base_methods = new Google_Service_Resource(
+        $this,
+        $this->serviceName,
+        '',
+        array(
+          'methods' => array(
+            <? foreach ($doc['methods'] as $method_name => $method): // resource,method ?>
+'<?=$method_name?>' => array(
+              'path' => '<?=$method['path']?>',
+              'httpMethod' => '<?=$method['httpMethod']?>',
+<? if ($method['parameters']): ?>
+              'parameters' => array(<?foreach ($method['parameters'] as $pname => $pval): // resource,method,parameter?>
+
+                '<?=$pname?>' => array(
+                  'location' => '<?=$pval['location']?>',
+                  'type' => '<?=$pval['type']?>',
+<? if ($pval['repeated']):?>
+                  'repeated' => true,
+<? endif;?>
+<? if ($pval['required']):?>
+                  'required' => true,
+<? endif;?>
+                ),<? endforeach; // resource,method,parameter ?>
+
+              ),
+<? else: ?>
+              'parameters' => array(),
+<? endif; ?>
+            ),<? endforeach; // resource,method ?>
+
+          )
+        )
+    );
+<? endif; ?>
+  }
+<? if ($doc['methods'])
+   foreach ($doc['methods'] as $method_name => $method): ?>
+  /**
+   * (<?=$method['id']?>)
+   *
+   * @param array $optParams Optional parameters.
+<? if ($method['parameters']): ?>
+   *
+<? foreach ($method['parameters'] as $param_name => $param): ?>
+   * @opt_param string <?=$param_name?>
+
+<? endforeach; ?>
+<? endif; ?>
+   * @return Google_Service_<?=$CapCan?>_<?=$method['response']['$ref']?>
+
+   */
+  public function <?=$method_name?>($optParams = array())
+  {
+    $params = array();
+    $params = array_merge($params, $optParams);
+    return $this->base_methods->call('<?=$method_name?>', array($params), "Google_Service_<?=$CapCan?>_<?=$method['response']['$ref']?>");
+  }
+<? endforeach; ?>
+}
---
+++ generator/functions.schemas.php
@@ -0,0 +1,209 @@
+<?php
+
+function rootSchema($name_key){
+  global $CapCan, $doc;
+  $node = &$GLOBALS['schemas'][$name_key];
+  $fn = rename_taboos(str_replace('_', '', $name_key));
+  assert(!$node['ClassName']);
+  $node['ClassName'] = $fn;
+
+  list($cc) = countChildren([$name_key]);
+
+  // Make a PHP
+  // if ($node['properties']) { // TODO Empty Objects?!?!?
+  if ($node['type'] == 'object') {
+    if (is_array($node['additionalProperties']) && 
+      count($node['additionalProperties']) == 1 || $node['additionalProperties']['type'] == 'any') {
+      return;
+    }
+
+    if (!is_dir(DEST. "$CapCan")) {
+      mkdir(DEST. "$CapCan", 0755, true);
+    }
+
+    ob_start();
+    include '_Model.php';
+    file_put_contents(DEST. "$CapCan/$fn.php", ob_get_clean());
+  }
+}
+
+function childSchema($keys) {
+  global $CapCan;
+  $node = getNodeReference($keys);
+
+  list($cc, $suffix) = countChildren($keys);
+  if ($cc > 1 || $suffix !== '') {
+    $fk = replace_labels($keys);
+    $fk = array_filter($fk, 'is_properties');
+    $fk = array_map('rename_taboos', $fk);
+    $fn = implode('', $fk);
+    $fn = str_replace('_', ' ', $fn);
+    $fn = ucwords($fn);
+    $fn = str_replace(' ', '', $fn);
+    $fn .= $suffix;
+    if (count($keys) == 2 && $keys[1] == 'items') {
+      $fn .= 'Items';
+    }
+    assert(!$node['ClassName']);
+    $node['ClassName'] = $fn;
+
+    if ($suffix == 'Element') {
+      $node = getNodeReference(array_merge($keys,['additionalProperties']));
+    }
+
+    ob_start();
+    include '_Model.php';
+    file_put_contents(DEST. "$CapCan/$fn.php", ob_get_clean());
+  }
+}
+
+function countChildren($keys) {
+  $node = getNodeReference($keys);
+
+  assert(($node['items'     ] && $node['properties'          ]) == false);
+  assert(($node['items'     ] && $node['additionalProperties']) == false);
+  assert(($node['properties'] && $node['additionalProperties']) == false);
+
+   // ['type'] == 'array') {
+  if ($node['items']) {
+    assert($node['type'] == 'array');
+    if (count($node['items']) == 2 && $node['items']['format']) {
+      return [0, ''];
+    }
+    childSchema(array_merge($keys, ['items']));
+    return [1, ''];
+  }
+
+  if ($node['properties']) {
+    foreach ($node['properties'] as $k => &$v) {
+      childSchema(array_merge($keys,['properties', $k]));
+    }
+    return [2, '']; // count($node['properties']);
+  }
+
+  if ($node['additionalProperties']['properties']) {
+    foreach ($node['additionalProperties']['properties'] as $k => &$v) {
+      childSchema(array_merge($keys, ['additionalProperties', 'properties', $k]));
+    }
+    return [count($node['additionalProperties']['properties']), 'Element'];
+  }
+  return [0, ''];
+}
+
+function getNodeReference($keys) {
+  $node = &$GLOBALS['schemas'];
+  foreach ($keys as $k) {
+    $node = &$node[$k];
+  }
+  return $node;
+}
+
+function is_properties($str) {
+  if ($str == 'items'               ) return false;
+  if ($str == 'properties'          ) return false;
+  if ($str == 'additionalProperties') return false;
+  return true;
+}
+
+function rename_taboos($str) {
+  if ($GLOBALS['TABOO'][$str]) {
+    return ucfirst($GLOBALS['doc']['name']) . ucfirst($str);
+  }
+  return ucfirst($str);
+}
+
+function replace_labels($arr) {
+  $i = array_search('additionalProperties', $arr);
+  if ($i !== false && $arr[$i+1] == 'properties') {
+
+    $arr[$i] = "Element";
+    unset($arr[$i+1]);
+    return $arr;
+  }
+
+  $i = array_search('properties', $arr);
+  if ($i !== false && $arr[$i+1] == 'items') {
+    $arr[$i] = "Items";
+    unset($arr[$i+1]);
+  }
+
+  $i = array_search('properties', $arr);
+  if ($i !== false && $arr[$i+1] == 'items') {
+    $arr[$i] = "Items";
+    unset($arr[$i+1]);
+  }
+  return $arr;
+}
+
+function ucStrip($str) {
+  $str = str_replace('-', ' ', $str);
+  $str = str_replace('_', ' ', $str);
+  $str = ucwords($str);
+  $str = str_replace(' ', '', $str);
+  $str = str_replace('@', '', $str);
+  return $str;
+}
+
+function map_internally($str){
+  $newstr = lcfirst(ucStrip($str));
+  $newstr = trim($newstr, '$');
+  if ($newstr != $str) return $newstr;
+
+  return '';
+}
+
+function itemSimplicity($items) {
+
+  if (count($items) == 2 && $items['format']) return true;
+
+  if ($items['enum']) return true;
+
+  if (count($items) == 1 && $items['type'] == 'string') return true;
+
+  if (count($items) == 1 && $items['$ref']
+  && !is_property_complex($GLOBALS['doc']['schemas'][$items['$ref']])) {
+    return true;
+  }
+
+  if (count($items) == 1 && $items['type'] == 'any') return true;
+  if ($items['items'] && count($items['items']) == 1 && $items['items']['type'] == 'any') return true;
+  if ($items['additionalProperties']['type'] == 'any') return true;
+  if ($items['items']) {
+    return itemSimplicity($items['items']);
+  }
+  return false;
+}
+
+function is_property_complex(&$property) {
+  if (isset($property['properties'])) {
+    return true;
+  }
+  if ($property['additionalProperties']['$ref']) {
+    
+    if ($property['additionalProperties']['$ref'] == 'JsonValue') {
+      return is_property_complex($GLOBALS['doc']['schemas'][$property['additionalProperties']['$ref']]);
+    }
+    return true;
+  }
+  if ($property['additionalProperties']['items']) {
+    return !itemSimplicity($property['additionalProperties']['items']);
+  }
+  
+  if ($property['items']) {
+    return !itemSimplicity($property['items']);
+  }
+  
+  if ($property['$ref']) {
+    return is_property_complex($GLOBALS['doc']['schemas'][$property['$ref']]);
+    if ($property['$ref'] == 'JsonObject') {
+      return is_property_complex($GLOBALS['doc']['schemas'][$property['$ref']]);
+    }
+    return true;
+  }
+
+  if ($property['DataType'] == 'map') {
+    return true;
+  }
+
+  return false;
+}
