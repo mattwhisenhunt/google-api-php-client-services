8f81b7db2af374546badef15bba396ae4ced0d55
Matthew Whisenhunt <matt.whisenhunt@gmail.com>
2018-10-30 09:28:48 -0700

    Add more tests, use data providers.

--- generator/tests/Google/Service/Generator/ResourceTest.php
+++ generator/tests/Google/Service/Generator/ResourceTest.php
@@ -22,24 +22,46 @@ use Google\Service\Generator\Resource;
 class ResourceTest extends \PHPUnit\Framework\TestCase {
   private $emptyNode = [];
 
-  function testGetClassName() {
-    $names = ['bidders',
-              'accounts',
-              'filterSets',
-              'filteredBids',
-              'creatives'];
-    $r = new Resource($names, $empty_node);
+  /**
+   * @dataProvider flatProvider
+   */
+  function testTemplateGetters($name, $node,
+    $exp_ClassName,
+    $exp_MemberName,
+    $exp_LiteralName)
+  {
+    $resource = new Resource($name, $node);
+    $this->assertEquals($exp_ClassName, $resource->getClassName());
+    $this->assertEquals($exp_MemberName, $resource->getMemberName());
+    $this->assertEquals($exp_LiteralName, $resource->getLiteralName());
 
-    $deep_node = [
-      'resources' => ['accounts' => [
-        'resources' => ['filterSets' => [
-          'resources' => ['filteredBids' => [
-            'resources' => ['creatives' => []]]]]]]]];
-    $rparent = new Resource(['bidders'], $deep_node);
-    $rdeep = $rparent->getAllResources()[4];
+    $this->assertEquals('list'.$exp_ClassName,  $resource->getFnName('list'));
+    $this->assertEquals('clone'.$exp_ClassName, $resource->getFnName('clone'));
+    $this->assertEquals('unset'.$exp_ClassName, $resource->getFnName('unset'));
+    $this->assertEquals('update',               $resource->getFnName('update'));
+    $this->assertEquals('encrypt',              $resource->getFnName('encrypt'));
+  }
 
-    $this->assertEquals('BiddersAccountsFilterSetsFilteredBidsCreatives', $r->getClassName());
-    $this->assertEquals('BiddersAccountsFilterSetsFilteredBidsCreatives', $rdeep->getClassName());
+  /**
+   * @dataProvider multiLevelProvider
+   */
+  function testTemplateGetters2($name, $node,
+    $exp_ClassName,
+    $exp_MemberName,
+    $exp_LiteralName)
+  {
+    $parent = new Resource($name, $node);
+    $arr = $parent->getAllResources();
+    $resource = array_pop($arr);
+    $this->assertEquals($exp_ClassName, $resource->getClassName());
+    $this->assertEquals($exp_MemberName, $resource->getMemberName());
+    $this->assertEquals($exp_LiteralName, $resource->getLiteralName());
+
+    $this->assertEquals('list'.$exp_ClassName,  $resource->getFnName('list'));
+    $this->assertEquals('clone'.$exp_ClassName, $resource->getFnName('clone'));
+    $this->assertEquals('unset'.$exp_ClassName, $resource->getFnName('unset'));
+    $this->assertEquals('update',               $resource->getFnName('update'));
+    $this->assertEquals('encrypt',              $resource->getFnName('encrypt'));
   }
 
   function testGetFnName() {
@@ -54,5 +76,147 @@ class ResourceTest extends \PHPUnit\Framework\TestCase {
     $this->assertEquals('clonePhotos', $r->getFnName('clone'));
     $this->assertEquals('unsetPhotos', $r->getFnName('unset'));
     $this->assertEquals('callPhotos', $r->getFnName('call'));
+  }
+
+  /**
+   * @dataProvider multiLevelProvider
+   */
+  function testGetAllResources($name, $node,
+    $exp_ClassName,
+    $exp_MemberName,
+    $exp_LiteralName,
+    $exp_count)
+  {
+    $r = new Resource($name, $node);
+    $this->assertCount($exp_count, $r->getAllResources());
+  }
+
+  /**
+   * @dataProvider withMethodProvider
+   */
+  function testCollectionMethods($name, $node,
+    $exp_count)
+  {
+    $r = new Resource($name, $node);
+    foreach ($r->getMemberNames() as $str) {
+      $this->assertTrue(is_string($str));
+    }
+    foreach ($r->getMembers() as $resource) {
+      $this->assertTrue(is_a($resource, 'Google\Service\Generator\Resource'));
+    }
+
+    $this->assertCount($exp_count, $r->getMembers());
+  }
+
+  function flatProvider() {
+    return [
+      [ ['accounts','containers','workspaces','built_in_variables'],
+        [],
+        'AccountsContainersWorkspacesBuiltInVariables',
+        'accounts_containers_workspaces_built_in_variables',
+        'built_in_variables',
+      ],
+      [ ['bidders','accounts','filterSets','filteredBids','creatives'],
+        [],
+        'BiddersAccountsFilterSetsFilteredBidsCreatives',
+        'bidders_accounts_filterSets_filteredBids_creatives',
+        'creatives',
+      ],
+      [ ['projects'],
+        [],
+        'Projects',
+        'projects',
+        'projects',
+      ],
+      [ ['v2'],
+        [],
+        'V2',
+        'v2',
+        'v2',
+      ],
+      [ ['userinfo','v2','me'],
+        [],
+        'UserinfoV2Me',
+        'userinfo_v2_me',
+        'me',
+      ],
+    ];
+  }
+
+  function multiLevelProvider() {
+    return [
+      [ ['accounts'],
+        ['resources' => ['containers' => [
+          'resources' => ['workspaces' => [
+            'resources' => ['built_in_variables' => []]]]]]],
+        'AccountsContainersWorkspacesBuiltInVariables',
+        'accounts_containers_workspaces_built_in_variables',
+        'built_in_variables',
+        4,
+      ],
+      [ ['bidders'],
+        ['resources' => ['accounts' => [
+          'resources' => ['filterSets' => [
+            'resources' => ['filteredBids' => [
+              'resources' => ['creatives' => []]]]]]]]],
+        'BiddersAccountsFilterSetsFilteredBidsCreatives',
+        'bidders_accounts_filterSets_filteredBids_creatives',
+        'creatives',
+        5,
+      ],
+      [ ['projects'],
+        [],
+        'Projects',
+        'projects',
+        'projects',
+        1,
+      ],
+      [ ['v2'],
+        [],
+        'V2',
+        'v2',
+        'v2',
+        1,
+      ],
+      [ ['userinfo'],
+        ['resources' => ['v2' => [
+          'resources' => ['me' => []]]]],
+        'UserinfoV2Me',
+        'userinfo_v2_me',
+        'me',
+        3,
+      ],
+    ];
+  }
+
+  function withMethodProvider() {
+    return [
+      [ ['accounts'], // one level deep
+        ['resources' => ['containers' => [
+          'methods' => ['list' => []], 'resources' => ['workspaces' => [
+            'resources' => ['built_in_variables' => []]]]]]],
+        1,
+      ],
+      [ ['bidders'], // four levels deep
+        ['resources' => ['accounts' => [
+          'resources' => ['filterSets' => [
+            'resources' => ['filteredBids' => [
+              'resources' => ['creatives' => [ 'methods' => ['list' => []]]]]]]]]]],
+        1,
+      ],
+      [ ['projects'], // direct member
+        ['methods'=> ['create'=>[],'get'=>[],'update'=>[],'delete'=>[]]],
+        1,
+      ],
+      [ ['v2'],
+        [],
+        0,
+      ],
+      [ ['userinfo'], // direct member and deeper
+        ['methods' => ['list' => []], 'resources' => ['v2' => [
+          'resources' => ['me' => ['methods' => ['list' => []]]]]]],
+        2,
+      ],
+    ];
   }
 }
